7a1149d7a5fef5b4920821a55655a6cd561d12c2
arpa12 <nihanarpa64@gmail.com>
2026-02-25 22:45:48 +0600

    changes commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       {/* <WhatIDo /> */}
       <FeaturedProjects />
       <WorkExperience />
-      <Skills />
+      {/* <Skills /> */}
       <CallToAction />
     </>
   );
